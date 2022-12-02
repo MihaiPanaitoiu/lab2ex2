27ca3c7686d8145a89b2aa4eccb97ce6276fdd44
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-12-02 15:14:06 +0200

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab2ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 2: Scrieti un program care va afisa factorialul unui numar n, n fiind citit
+                de la tastatura.
+             */
+
+            Console.WriteLine("Numarul n este: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result = result * i;
+            }
+
+            Console.WriteLine("Rezultatul este: " + result);
+        }
+    }
+}
